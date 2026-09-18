c852426e68bba628484bba03f18cc6db85180816
Wasee <mail.wasee@gmail.com>
2026-09-18 12:28:46 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
 self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Installed');
+  self.skipWaiting(); // บังคับให้ Service Worker ทำงานทันที
 });
 
 self.addEventListener('fetch', (e) => {
-  // ปล่อยผ่านการโหลดข้อมูลปกติ
+  // ปล่อยผ่าน (ดึงข้อมูลใหม่จากเน็ตเสมอ จะได้ไม่อืดเวลาเราแก้โค้ดฝั่ง GAS)
+  return; 
 });
